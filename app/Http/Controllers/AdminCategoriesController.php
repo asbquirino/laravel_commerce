b509b9ac17e4ce72d7322ecefaf17d56c0b7a34d
Antonio Sergio <antonio.quirino@campinas.sp.gov.br>
2016-10-03 12:51:47 -0300

    Criado o controller para a category

---
+++ app/Http/Controllers/AdminCategoriesController.php
@@ -0,0 +1,35 @@
+<?php namespace CodeCommerce\Http\Controllers;
+
+use CodeCommerce\Http\Requests;
+use CodeCommerce\Http\Controllers\Controller;
+use CodeCommerce\Category;
+
+use Illuminate\Http\Request;
+
+class AdminCategoriesController extends Controller
+{
+
+    private $categories;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct(Category $category)
+    {
+        $this->categories = $category;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        $categories = $this->categories->all();
+        return view('category', compact('categories'));
+    }
+
+}
