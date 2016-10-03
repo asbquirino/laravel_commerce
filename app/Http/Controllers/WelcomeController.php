d581d8381963aa7075314d10be9133c08921b804
Antonio Sergio <antonio.quirino@campinas.sp.gov.br>
2016-10-03 12:56:31 -0300

    Alterado o WelcomeController

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -1,36 +1,26 @@
 <?php namespace CodeCommerce\Http\Controllers;
 
-use CodeCommerce\Category;
+class WelcomeController extends Controller
+{
 
-class WelcomeController extends Controller {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
 
-	private $categories;
+    /**
+     * Show the application welcome screen to the user.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        return view('welcome');
+    }
 
-	/**
-	 * Create a new controller instance.
-	 *
-	 * @return void
-	 */
-	public function __construct(Category $category)
-	{
-		$this->middleware('guest');
-		$this->categories = $category;
-	}
-
-	/**
-	 * Show the application welcome screen to the user.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		return view('welcome');
-	}
-
-	public function exemplo()
-	{
-		$categories = $this->categories->all();
-
-		return view('exemplo', compact('categories'));
-	}
 }
