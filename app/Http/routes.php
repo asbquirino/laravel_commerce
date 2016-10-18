a33273b5e019c86ee17547ed75d904fe465f3446
ASBQuirino <asbquirino@yahoo.com.br>
2016-10-17 22:29:12 -0200

    Alterada as rotas para poder navegar nas funções criadas: Create,  Edit, Update, Delete, Store e Index category.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -39,3 +39,10 @@ Route::group(['prefix'=>'admin'], function(){
     });
 
 });
+
+Route::get('categories',['as'=>'categories', 'uses'=>'CategoriesController@index']);
+Route::post('categories',['as'=>'categories.store', 'uses'=>'CategoriesController@store']);
+Route::get('categories/create',['as'=>'categories.create', 'uses'=>'CategoriesController@create']);
+Route::get('categories/{id}/destroy',['as'=>'categories.destroy', 'uses'=>'CategoriesController@destroy']);
+Route::get('categories/{id}/edit',['as'=>'categories.edit', 'uses'=>'CategoriesController@edit']);
+Route::put('categories/{id}/update',['as'=>'categories.update', 'uses'=>'CategoriesController@update']);
