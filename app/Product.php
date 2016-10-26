dd18c0f463657c3204b137fdb96ee4bca67a3e14
Antonio Quirino <antonio.quirino@campinas.sp.gov.br>
2016-10-26 16:34:46 -0200

    Alterada a forma de visualização

--- app/Product.php
+++ app/Product.php
@@ -5,6 +5,12 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
 
-    protected $fillable = ['name', 'description', 'price', 'featured', 'recommended'];
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'featured',
+        'recommended'
+    ];
 
 }
