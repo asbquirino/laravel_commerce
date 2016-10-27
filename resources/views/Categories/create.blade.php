f2eacb3ed00ca0f6bfb0ccf6fe9dbe78c253fc3b
Antonio Quirino <antonio.quirino@campinas.sp.gov.br>
2016-10-27 14:12:08 -0200

    Criado o botão VOLTAR no formulário create da category

--- resources/views/Categories/create.blade.php
+++ resources/views/Categories/create.blade.php
@@ -27,6 +27,7 @@
 
         <div class="form-group">
             {!! Form::submit('Add Category', ['class'=>'btn btn-primary']) !!}
+            <a href="{{route('categories')}}" class="btn btn-default">Voltar</a>
         </div>
 
         {!! Form::close() !!}
