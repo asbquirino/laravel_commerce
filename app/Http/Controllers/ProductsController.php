39d55be50fad2db9b819f5a3efdb4cae5c1ac5e1
Antonio Quirino <antonio.quirino@campinas.sp.gov.br>
2016-11-09 09:39:28 -0200

    Adicionada as ações de Visualização, Create, Store e Destroy das imagens

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -5,9 +5,13 @@ use CodeCommerce\Product;
 use CodeCommerce\Http\Requests;
 use CodeCommerce\Http\Controllers\Controller;
 
+use CodeCommerce\ProductImage;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 
-class ProductsController extends Controller {
+class ProductsController extends Controller
+{
 
     private $productModel;
 
@@ -33,7 +37,7 @@ class ProductsController extends Controller {
         $input = $request->all();
         $product = $this->productModel->fill($input);
         $product->save();
-        return  redirect()->route('products');
+        return redirect()->route('products');
     }
 
     public function edit($id, Category $category)
@@ -52,7 +56,40 @@ class ProductsController extends Controller {
     public function destroy($id)
     {
         $this->productModel->find($id)->delete($id);
-        return  redirect()->route('products');
+        return redirect()->route('products');
+    }
+
+    public function images($id)
+    {
+        $product = $this->productModel->find($id);
+        return view('products.images', compact('product'));
+    }
+
+    public function createImage($id)
+    {
+        $product = $this->productModel->find($id);
+        return view('products.create_image', compact('product'));
+
+    }
+
+    public function storeImage(Requests\ProductImageRequest $request, $id, ProductImage $productImage)
+    {
+        $file = $request->file('image');
+        $extension = $file->getClientOriginalExtension();
+        $image = $productImage::create(['product_id' => $id, 'extension' => $extension]);
+        Storage::disk('public_local')->put($image->id . '.' . $extension, File::get($file));
+        return redirect()->route('products.images', ['id' => $id]);
+    }
+
+    public function destroyImage(ProductImage $productImage, $id)
+    {
+        $image = $productImage->find($id);
+        if (file_exists(public_path() . '/uploads/' . $image->id . '.' . $image->extension)) {
+            Storage::disk('public_local')->delete($image->id . '.' . $image->extension);
+        }
+        $product = $image->product;
+        $image->delete();
+        return redirect()->route('products.images', ['id' => $product->id]);
     }
 
 }
